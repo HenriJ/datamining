66964e00cf3ac2bf7d9cfaaf2c03fa329dc25ff2
Paul-Louis Ageneau <paul-louis@ageneau.org>
2011-03-10 19:14:28 +0100

    Added mysql support

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,20 @@
+import java.sql.SQLException;
+
 import distributions.*;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         int W = 800;
         int H = 600;
 
         Distribution r1 = new RectDistribution(10, 10, 100, 100);
         Distribution r2 = new RectDistribution(200, 20, 50, 50);
         Distribution r3 = new RectDistribution(100, 200, 150, 150);
-
         Distribution d = new MergedDistribution(r1, r2, r3);
 
-        NeuralGas gas = new NeuralGas(d, 3, 900000);
+        DbDistribution dbd = new DbDistribution("riton","datamining","datamining","errr");
+        
+        NeuralGas gas = new NeuralGas(dbd, 3, 900000);
 
         Canvas c = new Canvas("Nodes", gas, W, H, 30);
     }
--- src/distributions/DbDistribution.java
+++ src/distributions/DbDistribution.java
@@ -1,19 +1,69 @@
 package distributions;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class DbDistribution implements Distribution {
 
+	HashMap<Integer, double[]> map = new HashMap<Integer, double[]>();
+	ArrayList<double[]> list = null;
+	int dim = 0;
+	Random r = new Random();
 	
+	
+	public DbDistribution(String host, String database, String user, String password) throws SQLException {
+		
+		String url = "jdbc:mysql://"+host+"/"+database+"?user="+user;
+		if(!password.isEmpty()) url+= "&password="+password;
+		
+		Connection conn = DriverManager.getConnection(url);
+		
+		Statement stmt = conn.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM objects");
+		rs.next();
+		dim = rs.getInt(1);
+		rs.close();
+		
+		rs = stmt.executeQuery("SELECT ip,object,instances FROM stat");
+		while(rs.next()) {
+			
+			int ip = rs.getInt(1);
+			int object = rs.getInt(2);
+			int instances = rs.getInt(3);
+			
+			double[] table;
+			if(map.containsKey(ip)) table = map.get(ip);
+			else {
+				table = new double[dim];
+				map.put(ip,table);
+			}
+			
+			table[object] = (double)instances;
+		}
+		
+		rs.close();
+		stmt.close();
+		
+		list = new ArrayList<double[]>(map.values());
+	}
 	
 	@Override
 	public int dimension() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return dim;
 	}
 
 	@Override
 	public double[] generateVector() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return list.get(r.nextInt(dim));
 	}
 
 }
